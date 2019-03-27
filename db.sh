78603e25fd2dcc3133072f662a3e31ae63f2562b
senatanton <43574942+senatanton@users.noreply.github.com>
2019-03-27 23:02:04 +0300

    Create db.sh

---
+++ db.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+mysql -c -u test -ptest test_db -e "CREATE TABLE Customers (row1 INT, row2 INT);"
+for step in $(seq 0 1000)
+do
+   mysql -c -u test -ptest test_db -e "INSERT INTO Customers SET row1 = FLOOR(1000*RAND()), row2 = FLOOR(9999*RAND());"
+done

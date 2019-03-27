dbd912c2040be2346f11153b34e27f3952082c0d
senatanton <43574942+senatanton@users.noreply.github.com>
2019-03-27 15:29:58 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,11 @@
 #!/bin/sh
+wget https://raw.githubusercontent.com/senatanton/test_task/master/docker-compose.yml
+mkdir grafana
+wget https://raw.githubusercontent.com/senatanton/test_task/master/grafana.ini
+docker-compose up -d
+#проверка запущенного DB
+$id_mysql
+#наполнение базы
+docker exec -i -t $id_mysql mysql -c -u test -ptest test_db -e "CREATE TABLE Cusers (row1 INT, row2 INT);"
+ 
+for n in {1..1000}; do docker exec -i -t $id_mysql mysql -c -u test -ptest test_db -e "INSERT INTO Customers SET row1 = FLOOR(1000*RAND()), row2 = FLOOR(9999*RAND());";done > /dev/null

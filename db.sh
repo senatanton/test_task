6c7ad2b91e2c7b99a1b14e013ce4f6f6eda27ad0
senatanton <43574942+senatanton@users.noreply.github.com>
2019-03-27 23:18:54 +0300

    Update db.sh

--- db.sh
+++ db.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 sleep 50
-mysql -c -u test -ptest test_db -e "CREATE TABLE Customers (row1 INT, row2 INT);"
+mysql -u test -ptest test_db -e "CREATE TABLE Customers (row1 INT, row2 INT);"
 sleep 10
 for step in $(seq 0 1000)
 do
-   mysql -c -u test -ptest test_db -e "INSERT INTO Customers SET row1 = FLOOR(1000*RAND()), row2 = FLOOR(9999*RAND());"
+   mysql -u test -ptest test_db -e "INSERT INTO Customers SET row1 = FLOOR(1000*RAND()), row2 = FLOOR(9999*RAND());"
 done

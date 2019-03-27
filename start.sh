0f40bc432e3843ce05ae0e8f64d3db6b560d7526
senatanton <43574942+senatanton@users.noreply.github.com>
2019-03-27 16:12:31 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,7 @@ mkdir grafana
 wget https://raw.githubusercontent.com/senatanton/test_task/master/grafana.ini
 docker-compose up -d
 #проверка запущенного DB
-$id_mysql
+id_mysql=$( docker ps | grep test_db_senatskiy_ao | awk '{ print $1 }')
 #наполнение базы
 docker exec -i -t $id_mysql mysql -c -u test -ptest test_db -e "CREATE TABLE Cusers (row1 INT, row2 INT);"
- 
 for n in {1..1000}; do docker exec -i -t $id_mysql mysql -c -u test -ptest test_db -e "INSERT INTO Customers SET row1 = FLOOR(1000*RAND()), row2 = FLOOR(9999*RAND());";done > /dev/null

f4c416d2d002a73900cebcb68c39b0d2a0fb77ca
senatanton <43574942+senatanton@users.noreply.github.com>
2019-03-27 16:52:25 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,6 @@ docker-compose up -d
 #проверка запущенного DB
 id_mysql=$( docker ps | grep db_senatskiy_ao | awk '{ print $1 }')
 #наполнение базы
-docker exec -i -t $id_mysql mysql -c -u test -ptest test_db -e "CREATE TABLE Customers (row1 INT, row2 INT);"
+docker exec -i -t $id_mysql mysql -c -u test -ptest test_db -e "CREATE TABLE Customers (row1 INT, row2 INT);" > /dev/null
+sleep 10
 for n in {1..1000}; do docker exec -i -t $id_mysql mysql -c -u test -ptest test_db -e "INSERT INTO Customers SET row1 = FLOOR(1000*RAND()), row2 = FLOOR(9999*RAND());" > /dev/null;done

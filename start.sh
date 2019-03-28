9d257804ea4293d2f8de572a39e4d9866a7b7a88
senatanton <43574942+senatanton@users.noreply.github.com>
2019-03-28 10:13:46 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 wget https://raw.githubusercontent.com/senatanton/test_task/master/docker-compose.yml
 mkdir grafana mysql 
 mkdir ./mysql/conf/
+mkdir ./mysql/backup/
+wget -P ./mysql/backup/ https://raw.githubusercontent.com/senatanton/test_task/master/my2.sql
 wget -P ./mysql/conf/ https://raw.githubusercontent.com/senatanton/test_task/master/addco.conf
 wget https://raw.githubusercontent.com/senatanton/test_task/master/grafana.ini
 docker-compose up -d
@@ -9,6 +11,7 @@ docker-compose up -d
 id_mysql=$( docker ps | grep db_senatskiy_ao | awk '{ print $1 }')
 #наполнение базы
 docker exec -i -t $id_mysql bash -c "mysql_upgrade -uroot -p123456"
+docker exec -i -t $id_mysql bash -c "mysql --user=root -p123456 < /home/my2.sql"
 sleep 20
 docker exec -i -t $id_mysql mysql -c -u test -ptest test_db -e "CREATE TABLE Customers (row1 INT, row2 INT);"
 for step in $(seq 0 1000)
